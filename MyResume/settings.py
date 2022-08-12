99db2295c0f8c3ecee1ed40ddd1f3c1187577e5d
RaulSCoelho <raul.s.coelho@gmail.com>
2022-08-12 16:19:25 -0300

    staticfiles

--- MyResume/settings.py
+++ MyResume/settings.py
@@ -155,7 +155,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 django_heroku.settings(locals())
 
